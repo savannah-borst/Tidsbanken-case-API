06de1c22bd9634589d2b8ef33018c9e821c77857
savannah-borst <savannah.borst@gmail.com>
2022-04-05 10:40:32 +0200

    Initial commit

---
+++ src/main/java/com/tidsbankencaseapi/TidsbankenCaseApiApplication.java
@@ -0,0 +1,13 @@
+package com.tidsbankencaseapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TidsbankenCaseApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TidsbankenCaseApiApplication.class, args);
+    }
+
+}
