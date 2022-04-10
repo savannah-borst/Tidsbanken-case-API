bb669ea127dbcffd49b2a83b3368c9f51e42bef6
iljaasdhonre <iljaasd@outlook.com>
2022-04-10 13:07:10 +0200

    FEAT: retrieve user_info from Jwt

--- src/main/java/com/tidsbankencaseapi/Controllers/TestController.java
+++ src/main/java/com/tidsbankencaseapi/Controllers/TestController.java
@@ -1,15 +1,21 @@
 package com.tidsbankencaseapi.Controllers;
 
 import com.tidsbankencaseapi.Models.CommonResponse;
+import com.tidsbankencaseapi.Models.Employee;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+import java.util.Collections;
+import java.util.Hashtable;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @SecurityRequirement(name = "keycloak_implicit")
@@ -41,6 +47,13 @@ public class TestController {
         return "Hello protected default user";
     }
 
+    //Protected with authentication
+    @GetMapping("/employee/protected/user/role")
+    @PreAuthorize("hasRole('api-user')")
+    public String helloProtectedDefaultUserRole(){
+        return "Has role user";
+    }
+
     //Protected method to inspect JWT
     @GetMapping("/inspect")
     @PreAuthorize("hasAuthority('GROUP_admin')")
@@ -49,4 +62,32 @@ public class TestController {
         return ResponseEntity
                 .ok(new CommonResponse<>(principal.getClaims().get("given_name")));
     }
+
+    //Show the mapped information retrieved from the Jwt
+    @GetMapping("/userInfo")
+    public Map<String, Object> getUserInfo(@AuthenticationPrincipal Jwt principal){
+       Map<String, String> userMap = new Hashtable<String, String>();
+       userMap.put("user_uuid", principal.getClaimAsString("sub"));
+        userMap.put("user_name", principal.getClaimAsString("preferred_username"));
+        userMap.put("email", principal.getClaimAsString("email"));
+        userMap.put("first_name", principal.getClaimAsString("given_name"));
+        userMap.put("last_name", principal.getClaimAsString("family_name"));
+        userMap.put("roles", String.valueOf(principal.getClaimAsString("roles")));
+        return Collections.unmodifiableMap(userMap);
+    }
+
+    //Returns the entire principal object
+    @GetMapping("/principal")
+    public Principal getUser(Principal user){
+        return user;
+    }
+
+//    @PostMapping
+//    public ResponseEntity<Employee>addNewEmployee(@AuthenticationPrincipal Jwt principal){
+//        //check if employee exists by email
+//        //service.checkIfEmployeeExists(principal.getClaimAsString("email")
+//        //if true return bad request
+//        //if false create new employee based on principal
+//    }
+
 }
