4dc6e6fdb850dda227a63ab842bfc4ea4d6f4989
savannah-borst <savannah.borst@gmail.com>
2022-04-11 09:49:01 +0200

    fix: splitting name to firstname  + lastname

--- src/main/java/com/tidsbankencaseapi/Models/Employee.java
+++ src/main/java/com/tidsbankencaseapi/Models/Employee.java
@@ -18,7 +18,12 @@ public class Employee {
     @NotBlank
     @Size(max = 50)
     @Column(length = 50, nullable = false)
-    public String name;
+    public String first_name;
+
+    @NotBlank
+    @Size(max = 50)
+    @Column(length = 50, nullable = false)
+    public String last_name;
 
     @NotBlank
     @Size(max = 50)
