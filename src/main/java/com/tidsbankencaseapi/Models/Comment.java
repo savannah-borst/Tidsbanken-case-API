ab9c0e92a6becda47ba28d06e2579186f0fcb947
savannah-borst <savannah.borst@gmail.com>
2022-04-12 14:38:09 +0200

    fix: delete unused import

--- src/main/java/com/tidsbankencaseapi/Models/Comment.java
+++ src/main/java/com/tidsbankencaseapi/Models/Comment.java
@@ -4,7 +4,6 @@ import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 import java.util.Date;
-import java.util.List;
 
 @Entity
 public class Comment {
