057d1a3ef9654c660919951b3e95c659cc4e61c0
iljaasdhonre <iljaasd@outlook.com>
2022-04-13 00:00:51 +0200

    FEAT: status added to request

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,12 +2,12 @@ INSERT INTO employee (employee_id, first_name, last_name, email_address, is_admi
 INSERT INTO employee (employee_id, first_name, last_name, email_address, is_admin, profile_pic) VALUES ('ba953f00-a505-4504-9449-b17480e04d8b', 'Dianto', 'Bosman', 'testuser@tidsbanken.com', FALSE, 'neutral');
 INSERT INTO employee (employee_id, first_name, last_name, email_address, is_admin, profile_pic) VALUES ('il-1', 'Iljaas', 'Dhonre', 'Iljaas@gmail.com', FALSE, 'smile');
 
-INSERT INTO vacation_request (title, date_created, date_updated, period_end, period_start) VALUES ('request1', '2022-04-11T09:23:46.188Z', '2022-04-11T09:23:46.188Z', '2022-04-11T09:23:46.188Z', '2022-04-11T09:23:46.188Z');
-INSERT INTO vacation_request (title, date_created, date_updated, period_end, period_start) VALUES ('request2', '2022-04-11T09:23:46.188Z', '2022-04-11T09:23:46.188Z', '2022-04-11T09:23:46.188Z', '2022-04-11T09:23:46.188Z');
-INSERT INTO vacation_request (title, date_created, date_updated, period_end, period_start) VALUES ('request3', '2022-04-11T09:23:46.188Z', '2022-04-11T09:23:46.188Z', '2022-04-11T09:23:46.188Z', '2022-04-11T09:23:46.188Z');
-INSERT INTO vacation_request (title, date_created, date_updated, period_end, period_start) VALUES ('request4', '2022-04-11T09:23:46.188Z', '2022-04-11T09:23:46.188Z', '2022-04-11T09:23:46.188Z', '2022-04-11T09:23:46.188Z');
-INSERT INTO vacation_request (title, date_created, date_updated, period_end, period_start) VALUES ('request5', '2022-04-11T09:23:46.188Z', '2022-04-11T09:23:46.188Z', '2022-04-11T09:23:46.188Z', '2022-04-11T09:23:46.188Z');
-INSERT INTO vacation_request (title, date_created, date_updated, period_end, period_start) VALUES ('request6', '2022-04-11T09:23:46.188Z', '2022-04-11T09:23:46.188Z', '2022-04-11T09:23:46.188Z', '2022-04-11T09:23:46.188Z');
+INSERT INTO vacation_request (title, date_created, date_updated, period_end, period_start, status) VALUES ('request1', '2022-04-11T09:23:46.188Z', '2022-04-11T09:23:46.188Z', '2022-04-11T09:23:46.188Z', '2022-04-11T09:23:46.188Z', 'PENDING');
+INSERT INTO vacation_request (title, date_created, date_updated, period_end, period_start, status) VALUES ('request2', '2022-04-11T09:23:46.188Z', '2022-04-11T09:23:46.188Z', '2022-04-11T09:23:46.188Z', '2022-04-11T09:23:46.188Z', 'PENDING');
+INSERT INTO vacation_request (title, date_created, date_updated, period_end, period_start, status) VALUES ('request3', '2022-04-11T09:23:46.188Z', '2022-04-11T09:23:46.188Z', '2022-04-11T09:23:46.188Z', '2022-04-11T09:23:46.188Z', 'PENDING');
+INSERT INTO vacation_request (title, date_created, date_updated, period_end, period_start, status) VALUES ('request4', '2022-04-11T09:23:46.188Z', '2022-04-11T09:23:46.188Z', '2022-04-11T09:23:46.188Z', '2022-04-11T09:23:46.188Z', 'PENDING');
+INSERT INTO vacation_request (title, date_created, date_updated, period_end, period_start, status) VALUES ('request5', '2022-04-11T09:23:46.188Z', '2022-04-11T09:23:46.188Z', '2022-04-11T09:23:46.188Z', '2022-04-11T09:23:46.188Z', 'PENDING');
+INSERT INTO vacation_request (title, date_created, date_updated, period_end, period_start, status) VALUES ('request6', '2022-04-11T09:23:46.188Z', '2022-04-11T09:23:46.188Z', '2022-04-11T09:23:46.188Z', '2022-04-11T09:23:46.188Z', 'PENDING');
 
 INSERT INTO employee_vacationrequest SELECT 'e8307ff3-2dd5-4fbc-b0a3-44ddbb956cc4', 1;
 INSERT INTO employee_vacationrequest SELECT 'e8307ff3-2dd5-4fbc-b0a3-44ddbb956cc4', 2;
