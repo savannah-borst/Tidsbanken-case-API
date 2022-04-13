19a957471891716f472ed7a43ac1b7fcc2aac9ac
savannah-borst <savannah.borst@gmail.com>
2022-04-13 15:35:54 +0200

    feat: desc order comments from requests

--- src/main/java/com/tidsbankencaseapi/Repositories/CommentRepository.java
+++ src/main/java/com/tidsbankencaseapi/Repositories/CommentRepository.java
@@ -4,6 +4,10 @@ import com.tidsbankencaseapi.Models.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
+
+     List<Comment> getAllByVacationRequestRequestIdOrderByDateCreatedDesc(Integer requestId);
 }
