136eba3a2b0df6efac11820fc1b360239d5a3039
iljaasdhonre <iljaasd@outlook.com>
2022-04-14 17:17:01 +0200

    FEAT: Ineligble period added to dummydata

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -12,3 +12,7 @@ INSERT INTO vacation_request (title, date_created, date_updated, period_end, per
 INSERT INTO comment (date_created, date_updated, message, comment_owner_id, request_id) VALUES ('2022-04-11T09:23:46.188Z', '2022-04-11T09:23:46.188Z', 'vacation request for savannah', 'e8307ff3-2dd5-4fbc-b0a3-44ddbb956cc4', '1');
 INSERT INTO comment (date_created, date_updated, message, comment_owner_id, request_id) VALUES ('2022-04-11T09:23:46.188Z', '2022-04-11T09:23:46.188Z', 'vacation request for dianto', 'ba953f00-a505-4504-9449-b17480e04d8b', '1');
 INSERT INTO comment (date_created, date_updated, message, comment_owner_id, request_id) VALUES ('2022-04-11T09:23:46.188Z', '2022-04-11T09:23:46.188Z', 'vacation request for iljaas', 'il-1', '6');
+
+INSERT INTO ineligible_period (period_start, period_end) VALUES ('2022-02-11T09:23:46.188Z', '2022-04-11T09:23:46.188Z');
+INSERT INTO ineligible_period (period_start, period_end) VALUES ('2022-03-11T09:23:46.188Z', '2022-05-11T09:23:46.188Z');
+INSERT INTO ineligible_period (period_start, period_end) VALUES ('2022-04-11T09:23:46.188Z', '2022-06-11T09:23:46.188Z');
